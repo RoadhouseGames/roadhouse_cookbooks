e4bf755e8fd0a1ddd2ecb01c92884c6ff461da7b
David Gagnon <david@roadhousegames.com>
2012-09-05 18:17:25 -0300

    Added mwt-windows-test for some sweet, sweet chef script writing from Windows.

---
+++ cookbooks/mwt-windows-test/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: mwt-windows-test
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+log "Bahaha, hello from Windows and RubyMine! Because this is a billion times better than just SSH and vi, fool!"
