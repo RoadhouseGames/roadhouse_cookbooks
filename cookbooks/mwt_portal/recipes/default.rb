f7d49adc472b559e8f30fbdd2f0b6d4287e79186
RHG-George <george@roadhousegames.com>
2012-09-26 09:55:00 -0300

    Added configuration of the /etc/httpd/sites-enabled/mwtactics.conf file (changing the public root) and we now boot strap the application/services/init.sh script

--- cookbooks/mwt_portal/recipes/default.rb
+++ cookbooks/mwt_portal/recipes/default.rb
@@ -9,7 +9,30 @@
 
 rightscale_marker :begin
 
-log "  Testing configuration of mwt web portal"
+APACHE_CONFIG_DIR = node[:apache][:dir] + "/sites-enabled"
+WWW_DOCUMENT_ROOT = node[:app][:root]
+WWW_NEW_DOCUMENT_ROOT = WWW_DOCUMENT_ROOT + "/public"
+SERVICES_FOLDER = WWW_DOCUMENT_ROOT + "/application/services"
 
+log "  Configuring MW Tactics Web Portal"
+
+bash "configure_web_portal" do
+  cwd APACHE_CONFIG_DIR
+  code <<-EOH
+    echo "    Setting DocumentRoot via sed to #{WWW_NEW_DOCUMENT_ROOT}"
+    sed 's:#{WWW_DOCUMENT_ROOT}:#{WWW_NEW_DOCUMENT_ROOT}:' -i mwtactics.conf
+  EOH
+end
+
+log "  Executing MW Tactics Web Portal Initialization Script"
+
+bash "configure_web_portal_folders"
+  cwd SERVICES_FOLDER
+  code <<-EOH
+    bash init.sh
+  EOH
+end
+
+log "  Configuring application.php file"
 
 rightscale_marker :end
