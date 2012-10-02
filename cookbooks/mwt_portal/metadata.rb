4963fef0741400435858388e30ed3a29801c8e0b
RHG-George <george@roadhousegames.com>
2012-10-02 11:38:49 -0300

    Added more defaults and tweaked display names so they line up better when they are alphabetically sorted by RightScale

--- cookbooks/mwt_portal/metadata.rb
+++ cookbooks/mwt_portal/metadata.rb
@@ -16,7 +16,7 @@ attribute "mwt_portal",
           :type => "hash"
 
 attribute "mwt_portal/domain",
-          :display_name => "Portal Domain",
+          :display_name => "Domain",
           :description => "The domain for the portal. Example: 'mwtactics.com' or 'staging.mwtactics.com'.",
           :required => "required",
           :recipes => ["mwt_portal::default"]
@@ -24,13 +24,15 @@ attribute "mwt_portal/domain",
 attribute "mwt_portal/portal_hostname",
           :display_name => "Portal Hostname",
           :description => "The short hostname for the portal. Example: 'www'.",
-          :required => "required",
+          :required => "recommended",
+          :default => "www",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/game_hostname",
           :display_name => "Game Server Hostname",
           :description => "The short hostname for the game server. Example: 'game'.",
-          :required => "required",
+          :required => "recommended",
+          :default => "game",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/game_port",
@@ -43,7 +45,8 @@ attribute "mwt_portal/game_port",
 attribute "mwt_portal/game_client_url",
           :display_name => "Game Client URL",
           :description => "The URL where the game client lives.  Example 'game.mwtactics.com/Content/unity/MechWarriorTactics/MechWarriorTactics.unity3d'.",
-          :required => "required",
+          :required => "recommended",
+          :default => "game.mwtactics.com/Content/unity/MechWarriorTactics/MechWarriorTactics.unity3d",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/game_require_beta_key",
@@ -62,42 +65,45 @@ attribute "mwt_portal/game_shared_secret",
 attribute "mwt_portal/db_forum_schema_name",
           :display_name => "Forum Database Schema Name",
           :description => "The Forum database (schema) name. Example: 'mwt_ipb'.",
-          :required => "required",
+          :required => "recommended",
+          :default => "mwt_ipb",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/db_forum_user",
-          :display_name => "Database Forum User Name",
+          :display_name => "Forum Database User Account",
           :description => "The Forum database account.  Example 'cred:DBAPPLICATION_USER'.",
           :required => "required",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/db_forum_password",
-          :display_name => "Database Forum Password",
+          :display_name => "Forum Database User Password",
           :description => "The Forum database account password.  Example: 'cred:DBAPPLICATION_PASSWORD'.",
           :required => "required",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/contact_email",
-          :display_name => "Contact Email",
+          :display_name => "Email Contact Account",
           :description => "The contact email address for the portal. Example: 'vibrant-sysadmin@vibrant-software.com'.",
-          :required => "required",
+          :required => "recommended",
+          :default => "vibrant-sysadmin@vibrant-software.com",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/sender_email",
-          :display_name => "Sender Email",
+          :display_name => "EMail Sender Account",
           :description => "The sender email address for the portal. Example: 'noreply@mwtactics.com'.",
-          :required => "required",
+          :required => "recommended",
+          :default => "noreply@mwtactics.com",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/smtp_user",
-          :display_name => "SMTP user",
-          :description => "The SMTP account for the portal.",
+          :display_name => "EMail SMTP Account",
+          :description => "The SMTP account for the portal.  Example 'cred:EMAIL_SMTP_USER'",
           :required => "required",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/smtp_password",
-          :display_name => "SMTP Password",
-          :description => "The SMTP password for the portal.",
+          :display_name => "EMail SMTP Password",
+          :description => "The SMTP password for the portal.  Example 'cred:EMAIL_SMTP_PASSWORD'",
           :required => "required",
           :recipes => ["mwt_portal::default"]
 
@@ -126,57 +132,61 @@ attribute "mwt_portal/aws_access_key",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/aws_secret_key",
-          :display_name => "AWS Access Key",
+          :display_name => "AWS Secret Key",
           :description => "The AWS secret key. Example: 'cred:AWS_SECRET_ACCESS_KEY'.",
           :required => "required",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/cdn_fqdn",
-          :display_name => "CDN Fully Qualified Domain Name",
+          :display_name => "CDN Asset Fully Qualified Domain Name",
           :description => "The CDN host fully qualified domain name. Example: 'cdn.mwtactics.com'.",
-          :required => "required",
+          :required => "recommended",
+          :default => "cdn.mwtactics.com",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/cdn_user",
-          :display_name => "CDN User account",
+          :display_name => "CDN Asset User Account",
           :description => "The CDN user account.  Example: 'cred:CDN_ASSET_USER'.",
           :required => "required",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/cdn_password",
-          :display_name => "CDN Password",
+          :display_name => "CDN Asset User Password",
           :description => "The CDN user account password.  Example: 'cred:CDN_ASSET_PASSWORD'.",
           :required => "required",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/cdn_user_asset_folder",
-          :display_name => "CDN User Asset Folder",
-          :description => "The CDN user asset folder.  This is combined to create the CDN download URL.  Example: '/portal_vs/mwt'.",
-          :required => "required",
+          :display_name => "CDN Asset Base Folder",
+          :description => "The CDN asset folder.  This is combined to create the CDN download URL.  Example: '/portal_vs/mwt'.",
+          :required => "recommended",
+          :default => "/portal_vs/mwt",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/cdn_storage_fqdn",
           :display_name => "CDN Storage Fully Qualified Domain Name",
           :description => "The CDN storage fully qualified domain name.  Example: 'acronymgame.upload.akamai.com'.",
-          :required => "required",
+          :required => "recommended",
+          :default => "acronymgame.upload.akamai.com",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/cdn_storage_user",
-          :display_name => "CDN Storage User",
+          :display_name => "CDN Storage User Account",
           :description => "The CDN storage user account.  Example: 'cred:CDN_STORAGE_USER'.",
           :required => "required",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/cdn_storage_password",
-          :display_name => "CDN Storage Password",
+          :display_name => "CDN Storage User Password",
           :description => "The CDN storage user account password.  Example: 'cred:CDN_STORAGE_PASSWORD'.",
           :required => "required",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/cdn_storage_asset_folder",
-          :display_name => "CDN Storage Asset Folder",
+          :display_name => "CDN Storage Base Folder",
           :description => "The CDN storage asset folder.  Example: '/167367/portal'.",
-          :required => "required",
+          :required => "recommended",
+          :default => "/167367/portal",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/memcache_fqdn",
@@ -188,7 +198,8 @@ attribute "mwt_portal/memcache_fqdn",
 attribute "mwt_portal/memcache_port",
           :display_name => "Memcache Port",
           :description => "The port for the memcache server. Example: '11211'.",
-          :required => "required",
+          :required => "recommended",
+          :default => "11211",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/playspan_id",
@@ -234,7 +245,7 @@ attribute "mwt_portal/playspan_featured_sub",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/playspan_product_id_prefix",
-          :display_name => "Playspan ",
+          :display_name => "Playspan Product ID",
           :description => "Playspan product ID prefix.  Example: 'premiumaccount1' where you want values premiumaccount1 to premiumaccount3.",
           :required => "required",
           :recipes => ["mwt_portal::default"]
@@ -254,7 +265,8 @@ attribute "mwt_portal/playspan_cbills_code",
 attribute "mwt_portal/google_analytics_account",
           :display_name => "Google Analytics Account",
           :description => "The Google Analytics Account ID. Example: '' in Dev or 'UA-28605391-1' in Live.",
-          :required => "required",
+          :required => "recommended",
+          :default => "UA-28605391-1",
           :recipes => ["mwt_portal::default"]
 
 attribute "mwt_portal/web_purify_key",
