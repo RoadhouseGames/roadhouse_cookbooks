d5c4e5c8f05f1ecde23a5f39354571e0ba66a68a
RHG-George <george@roadhousegames.com>
2012-10-09 17:38:02 -0300

    Updated cronjob default recipe

--- cookbooks/mwt_portal_cronjobs/recipes/default.rb
+++ cookbooks/mwt_portal_cronjobs/recipes/default.rb
@@ -15,30 +15,14 @@ class Chef::Recipe
   include RightScale::App::Helper
 end
 
-# Get all attached app servers
-vhosts(node[:lb][:vhost_names]).each do |vhost_name|
+run_remote_recipe("mwt_portal:playspan_complete_pending_requests")
 
-  log "  Getting all attached servers for: " + vhost_name
-
-  attached_servers = get_attached_servers(vhost_name)
-
-  attached_servers.each do |current_server|
-    log "  Found: " + current_server
-  end
+# Run the specified recipe on a single remote app server
+def run_remote_recipe(recipe_name)
+  remote_recipe recipe_name do
+  recipe "#{recipe_name}"
+  recipients_tags "appserver:active=true"
+  scope :single
 end
-
-############
-#attrs = {:app => Hash.new}
-#attrs[:app][:lb_ip] = node[:cloud][:private_ips][0]
-
-#vhosts(node[:lb][:vhost_names]).each do | vhost_name |
-#  remote_recipe "Update app servers firewall" do
-#    recipe "app::handle_loadbalancers_allow"
-#    recipients_tags "loadbalancer:#{vhost_name}=app"
-#    attributes attrs
-#  end
-#end
-############
-
 
 rightscale_marker :end
