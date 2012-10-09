b430b4a15f4bfd9667f18e380c15dea4c098a542
RHG-George <george@roadhousegames.com>
2012-10-09 18:12:17 -0300

    Fixed syntax error

--- cookbooks/mwt_portal_cronjobs/recipes/default.rb
+++ cookbooks/mwt_portal_cronjobs/recipes/default.rb
@@ -18,9 +18,10 @@ run_remote_recipe("mwt_portal:playspan_complete_pending_requests")
 # Run the specified recipe on a single remote app server
 def run_remote_recipe(recipe_name)
   remote_recipe recipe_name do
-  recipe "#{recipe_name}"
-  recipients_tags "appserver:active=true"
-  scope :single
+    recipe "#{recipe_name}"
+    recipients_tags "appserver:active=true"
+    scope :single
+  end
 end
 
 rightscale_marker :end
