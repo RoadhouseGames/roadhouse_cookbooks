88bf511ccd1dfaa63c5824888d8a85fcaf285541
RHG-George <george@roadhousegames.com>
2012-10-19 09:46:09 -0300

    added more logging to the update recipe

--- cookbooks/mwt_portal/recipes/update_game_client_url.rb
+++ cookbooks/mwt_portal/recipes/update_game_client_url.rb
@@ -10,13 +10,17 @@
 rightscale_marker :begin
 
 CONFIG_FOLDER = node[:app][:root] + "/application/configs"
+CONFIG_FILE = "application.php"
 GAME_CLIENT_URL = node[:mwt_portal][:game_client_url]
 
+
 log "Changing Game Client URL"
+log "  Editing file: #{CONFIG_FOLDER}/#{CONFIG_FILE}"
+log "  Setting URL to: #{GAME_CLIENT_URL}"
 bash "editing game client url" do
   cwd CONFIG_FOLDER
   code <<-EOH
-    sed "s!\$REGISTRY->game->client_url = '.*';!\$REGISTRY->game->client_url = 'http://#{GAME_CLIENT_URL}';!" -i application.php
+    sed "s!\$REGISTRY->game->client_url = '.*';!\$REGISTRY->game->client_url = 'http://#{GAME_CLIENT_URL}';!" -i #{CONFIG_FILE}
   EOH
 end
 
